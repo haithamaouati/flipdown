961cfe2a91425be2b42a7079d8507bce2085018d
Haitham Aouati <55059470+haithamaouati@users.noreply.github.com>
2024-03-28 19:19:01 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   // Unix timestamp (in seconds) to count down to
-  var twoDaysFromNow = (new Date().getTime() / 1000) + (86400 * 2) + 1;
-
+  var targetDate = new Date('09/06/2024').getTime() / 1000;
+  
   // Set up FlipDown
-  var flipdown = new FlipDown(twoDaysFromNow)
+  var flipdown = new FlipDown(targetDate)
 
     // Start the countdown
     .start()
